3a1eb9716f22fe678a645bbe54dcfa74be9a3623
Asbert75 <erik_olsson_97@hotmail.com>
2019-01-17 19:49:25 +0100

    Added build folder

---
+++ build/precache-manifest.a35ac1462fd59921f8737c68890654dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6612740aae5c95dc1d1d29dad1d76e7",
+    "url": "/static/media/border.e6612740.png"
+  },
+  {
+    "revision": "f1d65d59d0fe9f06940b9a5a9fe0c66b",
+    "url": "/static/media/background.f1d65d59.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "df911d2c25b3e75fd6d6",
+    "url": "/static/js/main.df911d2c.chunk.js"
+  },
+  {
+    "revision": "460a770354e2b7c58bfa",
+    "url": "/static/js/1.460a7703.chunk.js"
+  },
+  {
+    "revision": "df911d2c25b3e75fd6d6",
+    "url": "/static/css/main.419a97bc.chunk.css"
+  },
+  {
+    "revision": "a886c1eb5415d129ea485a58be137205",
+    "url": "/index.html"
+  }
+];
